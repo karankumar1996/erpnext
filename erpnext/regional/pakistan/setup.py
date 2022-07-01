d4959b08e9c57a2a2869853a42c488b7372f9b55
karankumar1996 <98558244+karankumar1996@users.noreply.github.com>
2022-07-01 10:36:16 +0500

    revert changes

--- erpnext/regional/pakistan/setup.py
+++ erpnext/regional/pakistan/setup.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from attr import field
 import frappe, os, json
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 from frappe.custom.doctype.property_setter.property_setter import make_property_setter
@@ -10,25 +8,27 @@ def setup(company=None, patch=True):
 	# Company independent fixtures should be called only once at the first company setup
 	if patch or frappe.db.count("Company", {"country": "Pakistan"}) <= 1:
 		setup_company_independent_fixtures(patch=patch)
-  
+
 # TODO: for all countries
 def setup_company_independent_fixtures(patch=False):
 	make_custom_fields()
- 
+
 def make_custom_fields(update=True):
 	custom_fields = get_custom_fields()
 	create_custom_fields(custom_fields, update=update)
- 
+
 def get_custom_fields():
+
 	custom_fields = {
-		"Item":	[
+		"Item": [
 			dict(
-				fieldname="ddct_code",
-				label="DDCT Code",
+				fieldname="hex_code",
+				label="Hex Code",
 				fieldtype="Data",
 				insert_after="item_group",
-			),
+			)
 		]
+
 	}
 
 	return custom_fields
